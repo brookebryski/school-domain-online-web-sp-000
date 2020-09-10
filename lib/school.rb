7b1604afff518f2d0820957e274f31e2d656b661
chic-state-0895 <brookeabryski@gmail.com>
2020-09-10 16:57:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+  end
+
+  roster["name"] = []
+  roster["name"] << "grade"
+
+  roster
+  => {"name" => ["grade"]}
+
+  def add_student(name, grade)
+  end
+
+end
